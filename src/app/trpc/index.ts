8450fb45b13fbaf0daf0b38d50cac94e3f8ca16c
saqlain <2mesaqlain@gmail.com>
2024-01-18 20:00:43 +0500

    3rd half started

--- src/app/trpc/index.ts
+++ src/app/trpc/index.ts
@@ -1,18 +1,18 @@
 import { z } from 'zod';
 import { publicProcedure, router } from './trpc';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+    authCallback: publicProcedure.query(async () => {
+        const {getUser} = getKindeServerSession()
+        const user  =  await getUser();
+
+        if (!user?.id || !user.email) throw new TRPCError({code: 'UNAUTHORIZED'})
+
+        // check if the user exists in the database ==> 2:03
+
+    })
 });
 
 // export type definition of API
